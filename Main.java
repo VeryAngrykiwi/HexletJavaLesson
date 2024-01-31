945dd391f8d2d188a9aa20699563bcdc41d1fa4d
VeryAngrykiwi <129200338+VeryAngrykiwi@users.noreply.github.com>
2024-01-31 08:50:16 +0000

    Greeting

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+public class Main {
+
+    public static void main(String[] args){
+      System.out.println("Hey, Kiwi proggrammer Java, Wow! i'ts amazing!");
+    }  
   }
-}
